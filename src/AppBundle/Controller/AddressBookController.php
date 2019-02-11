bc11d5db7a9fe3953c100db37a4a711e2663dbb7
Morayo <mbamgbose@accounteer.com>
2019-02-11 21:29:08 +0100

    initial app setup

---
+++ src/AppBundle/Controller/AddressBookController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AddressBookController extends Controller
+{
+
+    public function editAction(Request $request)
+    {
+        return $this->render('@App/AddressBook/edit.html.twig' );
+    }
+}
